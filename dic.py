8eeeedfaee95e10a2504bdf146e1491a0da1c4e3
SoouLAr <61475205+SoouLAr@users.noreply.github.com>
2020-03-18 15:03:27 +0100

    Update dic.py

--- dic.py
+++ dic.py
@@ -1,9 +1,12 @@
+#Create the board
 board = ["_", "_", "_",
          "_", "_", "_",
          "_", "_", "_", ]
+
+#Create some variables that wil help us for tun exchange
 i=1
 turnip = 0
-
+#Create a function that will check for winner or draw
 def kontrollori():
     if board[0]+board[1]+board[2]=='xxx':
         print ("fiton x")
@@ -53,7 +56,9 @@ def kontrollori():
     if board[2]+board[4]+board[6]=='ooo':
         print ("fiton o")
         quit()
-
+     else :
+         print ("Draw")
+#Create a turn for each player
 def turni():
     global  turnip
     if i % 2 == 0:
@@ -61,11 +66,13 @@ def turni():
     else :
         turnip = 'x'
     print(turnip)
+#Create a diplayed board
 def display_board():
     print(board[0], "|" + board[1], "|" + board[2])
     print(board[3], "|" + board[4], "|" + board[5])
     print(board[6], "|" + board[7], "|" + board[8])
-# vendosja e pikave
+# Where players wants to move/play
+#We have two player (Player X) and (Player O)
 def player_x():
     poziconi = int(input(": "))
     poziconi = poziconi - 1
@@ -74,7 +81,7 @@ def player_o():
     poziconi = int(input(": "))
     poziconi = poziconi - 1
     board[poziconi] = 'o'
-# rregulli i turnit
+#Tur exchange
 def game():
     global turnip,i
     if i <= 9:
@@ -87,7 +94,7 @@ def game():
     else:
         player_o()
     display_board()
-
+#Create an infinite loop that keeps our game going on
 while 1 < 2:
     game()
     kontrollori()
